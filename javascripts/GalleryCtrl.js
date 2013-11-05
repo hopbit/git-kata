6abd8392016bc9cdac9d50e90756c900c36339e4
Lukasz Siwinski <lsiwinski@gmail.com>
2013-11-05 22:41:09 +0100

    json with photos urls for carousel (from v1)

---
+++ javascripts/GalleryCtrl.js
@@ -0,0 +1,51 @@
+function GalleryCtrl($scope) {
+	$scope.photos = [
+			{
+				url : 'https://lh5.googleusercontent.com/-1uCJAIRN8dA/UU9je_BiulI/AAAAAAAAAP4/FCNx9WwV8lg/w858-h572-no/IMG_6553.JPG'
+			},
+			{
+				url : 'https://lh3.googleusercontent.com/-vIT_AsPQvb8/UU9kIuP1DHI/AAAAAAAAAUk/czMj9eEbDbA/w858-h572-no/IMG_6594.JPG'
+			},
+			{
+				url : 'https://lh6.googleusercontent.com/-yPwKKQVFE-c/UU9kMU9pMhI/AAAAAAAAAVA/7g4HqFXiL1c/w858-h572-no/IMG_6598.JPG'
+			},
+			{
+				url : 'https://lh6.googleusercontent.com/-A1B1ipvFNd0/UU9kNta8QMI/AAAAAAAAAVE/L74rQBoQLfI/w858-h572-no/IMG_6599.JPG'
+			},
+			{
+				url : 'https://lh5.googleusercontent.com/-ekMGNXSHatM/UU9kigOz4ZI/AAAAAAAAAXc/M8mA4PkBnTw/w858-h572-no/IMG_6618.JPG'
+			},
+			{
+				url : 'https://lh3.googleusercontent.com/-CAKFlGV1GrM/UU9lH1pIsKI/AAAAAAAAAb0/myrExdGgxuY/w858-h572-no/IMG_6655.JPG'
+			},
+			{
+				url : 'https://lh3.googleusercontent.com/-VasrClngl_g/UU9lJN57eEI/AAAAAAAAAng/RpJ_iTLsTeg/w858-h572-no/IMG_6656.JPG'
+			},
+			{
+				url : 'https://lh6.googleusercontent.com/-5Anu852qfq4/UU9lGB7So9I/AAAAAAAAAbk/Ir6hA6UTgdc/w858-h572-no/IMG_6652.JPG'
+			},
+			{
+				url : 'https://lh3.googleusercontent.com/-gHfyotsHl78/UU9ilaTaP_I/AAAAAAAAAJM/QyA0vLP4mVE/w858-h572-no/IMG_6495.JPG'
+			},
+			{
+				url : 'https://lh4.googleusercontent.com/-Ii7dPObZVdg/UU9ipsr6lII/AAAAAAAAAJs/-auXwxTcJVs/w858-h572-no/IMG_6499.JPG'
+			},
+			{
+				url : 'https://lh5.googleusercontent.com/-_PCZ2mDLPcE/UU9jX7DKzRI/AAAAAAAAAPE/-5w2K5ln4tY/w858-h572-no/IMG_6547.JPG'
+			},
+			{
+				url : 'https://lh4.googleusercontent.com/-qps1LQ-BdpM/UU9XGWL7xaI/AAAAAAAADa8/dHqDxr2bC74/w858-h572-no/IMG_8340.JPG'
+			},
+			{
+				url : 'https://lh3.googleusercontent.com/-St6eKhl0few/UU2KPL5a45I/AAAAAAAAKXA/GKEL-13jU94/w763-h572-no/IMG_1757.JPG'
+			},
+			{
+				url : 'https://lh3.googleusercontent.com/-TI8ed1J2J0U/UU3V9-mnbGI/AAAAAAAAKdE/YiR6e_ZDrqg/w763-h572-no/IMG_1800.JPG'
+			},
+			{
+				url : 'https://lh6.googleusercontent.com/-Qfz7TZQ0Acg/UU4Wza8T0YI/AAAAAAAAKeo/jHXabrQqhmA/w763-h572-no/IMG_1814.JPG'
+			},
+			{
+				url : 'https://lh4.googleusercontent.com/-yv6bxdF-gtE/UU2R6gLtlcI/AAAAAAAAKb8/enmF0C01vlI/w763-h572-no/IMG_1779.JPG'
+			} ];
+}
