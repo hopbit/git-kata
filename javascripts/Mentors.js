050964743080f37f7b657f146978515e80671b93
Lukasz Siwinski <lsiwinski@gmail.com>
2013-11-26 19:35:43 +0100

    don't show images if they're not available

--- javascripts/Mentors.js
+++ javascripts/Mentors.js
@@ -2,49 +2,56 @@ function Mentors($scope) {
 // ą ć ę ł ń ó ś ź ż
 	$scope.mentors = [ 
 	{
-		id : "piotr-betkier", name : "Piotr Betkier", homepage : "http://pbetkier.blogspot.com",
+		id : "piotr-betkier", name : "Piotr Betkier", homepage : "http://pbetkier.blogspot.com", img : "true",
 		bio : "I am a software developer who loves working in agile environments and creating clean code that works. I am not afraid of choosing the right tool for the job, and so far those were mostly Java and Python technologies. Git is in my opinion the right tool for version control currently. I was born perfectionist, but taught myself pragmatism over time. When not developing software, I co-organize PyWaw[1] meetings and spend my time actively. I do my commits in Allegro Group. [1] http://pywaw.org"
 	},
 	{
-		id : "piotr-jasiun", name : "Piotr Jasiun", homepage : "#",
+		id : "piotr-jasiun", name : "Piotr Jasiun", homepage : "#", img : "true",
 		bio : "I started my career as a Freelance Web Designer then switched to mobiles as an Android Developer in Polidea. I worked in CERN integrating C++ SCADA systems with JavaScript. I’m currently a JavaScript Developer on the CKEditor open source project. I love working in small companies with people who are passionate about the high-tech industry. I’m astonished by how quickly technology is changing these days, which is why I’m an advocate of Agile Methodologies. I also have a fondness for application usability. I caught the git bug after the first Kata conference and I intend to convey this interest onto others."
 	},	
 	{
-		id : "rafal-krzewski", name : "Rafał Krzewski",	homepage : "http://www.ohloh.net/accounts/rkrzewski",
+		id : "rafal-krzewski", name : "Rafał Krzewski",	homepage : "http://www.ohloh.net/accounts/rkrzewski", img : "true",
 		bio : "I co-run a small IT company for over 10 years. In such environment I had the chance to gain skills in various areas of software life cycle from design, project management, development, build engineering, continuous integration all the way to deployment and maintenance. I have worked with CVS and SVN before switching to git. I was responsible for migrating our repositories, and training my coworkers to use the new revision management tools. Eclipse is my IDE of choice when doing Java / Scala development. While I prefer to use git on command line for more advanced operations, I find EGit - the git client integrated in Eclipse to be adequate and quite convenient for the bread-and-butter commit - fetch - rebase - push workflow. In my session, I'm going to demonstrate how to install, configure and use EGit for the tasks it's best suited for."
 	},
 	{
-		id : "jakub-nabrdalik",	name : "Jakub Nabrdalik", homepage : "#",
-		bio : "---"
+		id : "jakub-nabrdalik",	name : "Jakub Nabrdalik", homepage : "#", img : "false",
+		bio : "TBD"
 	},
 	{
-		id : "grzegorz-kubiak", name : "Grzegorz Kubiak", homepage : "#",
-		bio : "---"
+		id : "grzegorz-kubiak", name : "Grzegorz Kubiak", homepage : "#", img : "false",
+		bio : "TBD"
 	},
 	{
-		id : "justyna-sadlo", name : "Justyna Sadło", homepage : "#", 
+		id : "justyna-sadlo", name : "Justyna Sadło", homepage : "#", img : "false",
 		bio : "-"
 	},
 	{
-		id : "krzysztof-adamski", name : "Krzysztof Adamski", homepage : "#",	bio : "-"
+		id : "krzysztof-adamski", name : "Krzysztof Adamski", homepage : "#", img : "false",
+		bio : "-"
 	},
 	{
-		id : "lukasz-szczesny", name : "Łukasz Szczęsny", homepage : "#",	bio : "-"
+		id : "lukasz-szczesny", name : "Łukasz Szczęsny", homepage : "#", img : "false",	
+		bio : "-"
 	},
 	{
-		id : "mariusz-smykula", name : "Mariusz Smykuła", homepage : "#",	bio : "-"
+		id : "mariusz-smykula", name : "Mariusz Smykuła", homepage : "#", img : "false",	
+		bio : "-"
 	},
 	{
-		id : "michal-bareja", name : "Michał Bareja", homepage : "#",	bio : "-"
+		id : "michal-bareja", name : "Michał Bareja", homepage : "#", img : "false",	
+		bio : "-"
 	},
 	{
-		id : "michal-lewandowski", name : "Michał Lewandowski", homepage : "#",	bio : "-"
+		id : "michal-lewandowski", name : "Michał Lewandowski", homepage : "#", img : "false",	
+		bio : "-"
 	},
 	{
-		id : "mateusz-kaczmarek", name : "Mateusz Kaczmarek", homepage : "#",	bio : "-"
+		id : "mateusz-kaczmarek", name : "Mateusz Kaczmarek", homepage : "#", img : "false",
+		bio : "-"
 	},
 	{
-		id : "pawel-sanjuan-szklarz", name : "Paweł Sanjuan Szklarz", homepage : "#",	bio : "-"
+		id : "pawel-sanjuan-szklarz", name : "Paweł Sanjuan Szklarz", homepage : "#", img : "false",
+		bio : "-"
 	}
 	];
 	
