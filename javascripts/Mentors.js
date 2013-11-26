01849f3b6a5186590b0b395cdff013eab6dbae82
Lukasz Siwinski <lsiwinski@gmail.com>
2013-11-26 17:11:17 +0100

    Piotr Jasiun added as mentor

--- javascripts/Mentors.js
+++ javascripts/Mentors.js
@@ -6,7 +6,13 @@ function Mentors($scope) {
 		name : "Piotr Betkier",
 		homepage : "#",
 		bio : "I am a software developer who loves working in agile environments and creating clean code that works. I am not afraid of choosing the right tool for the job, and so far those were mostly Java and Python technologies. Git is in my opinion the right tool for version control currently. I was born perfectionist, but taught myself pragmatism over time. When not developing software, I co-organize PyWaw[1] meetings and spend my time actively. I do my commits in Allegro Group. [1] podlinkujcie proszę pywaw.org"
-	}
+	},
+	{
+		id : "piotr-jasiun",
+		name : "Piotr Jasiun",
+		homepage : "#",
+		bio : "I started my career as a Freelance Web Designer then switched to mobiles as an Android Developer in Polidea. I worked in CERN integrating C++ SCADA systems with JavaScript. I’m currently a JavaScript Developer on the CKEditor open source project. I love working in small companies with people who are passionate about the high-tech industry. I’m astonished by how quickly technology is changing these days, which is why I’m an advocate of Agile Methodologies. I also have a fondness for application usability. I caught the git bug after the first Kata conference and I intend to convey this interest onto others."
+	}	
 	];
 	
 }
