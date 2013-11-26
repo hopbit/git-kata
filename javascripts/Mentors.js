42e3c59e53cc12053b16209ba61d434cf8ca7c28
Lukasz Siwinski <lsiwinski@gmail.com>
2013-11-27 00:01:11 +0100

    lil' update on bio

--- javascripts/Mentors.js
+++ javascripts/Mentors.js
@@ -3,7 +3,7 @@ function Mentors($scope) {
 	$scope.mentors = [ 
 	{
 		id : "piotr-betkier", name : "Piotr Betkier", homepage : "http://pbetkier.blogspot.com", img : "true",
-		bio : "I am a software developer who loves working in agile environments and creating clean code that works. I am not afraid of choosing the right tool for the job, and so far those were mostly Java and Python technologies. Git is in my opinion the right tool for version control currently. I was born perfectionist, but taught myself pragmatism over time. When not developing software, I co-organize PyWaw[1] meetings and spend my time actively. I do my commits in Allegro Group. [1] <a href='http://pywaw.org'>pywaw.org</a>"
+		bio : "I am a software developer who loves working in agile environments and creating clean code that works. I am not afraid of choosing the right tool for the job, and so far those were mostly Java and Python technologies. Git is in my opinion the right tool for version control currently. I was born perfectionist, but taught myself pragmatism over time. When not developing software, I co-organize <a href='http://pywaw.org'>PyWaw</a> meetings and spend my time actively. I do my commits in Allegro Group."
 	},
 	{
 		id : "piotr-jasiun", name : "Piotr Jasiun", homepage : "#", img : "true",
