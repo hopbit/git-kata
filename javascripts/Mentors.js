732a0a52fa40e1500c84af07a2dccf1ee1665c14
Lukasz Siwinski <lsiwinski@gmail.com>
2013-11-26 17:34:23 +0100

    minor 'refuctoring'

--- javascripts/Mentors.js
+++ javascripts/Mentors.js
@@ -5,7 +5,7 @@ function Mentors($scope) {
 		id : "piotr-betkier",
 		name : "Piotr Betkier",
 		homepage : "http://pbetkier.blogspot.com",
-		bio : "I am a software developer who loves working in agile environments and creating clean code that works. I am not afraid of choosing the right tool for the job, and so far those were mostly Java and Python technologies. Git is in my opinion the right tool for version control currently. I was born perfectionist, but taught myself pragmatism over time. When not developing software, I co-organize PyWaw[1] meetings and spend my time actively. I do my commits in Allegro Group. [1] podlinkujcie proszę pywaw.org"
+		bio : "I am a software developer who loves working in agile environments and creating clean code that works. I am not afraid of choosing the right tool for the job, and so far those were mostly Java and Python technologies. Git is in my opinion the right tool for version control currently. I was born perfectionist, but taught myself pragmatism over time. When not developing software, I co-organize PyWaw[1] meetings and spend my time actively. I do my commits in Allegro Group. [1] http://pywaw.org"
 	},
 	{
 		id : "piotr-jasiun",
