f5a5b686955055536c1c76fc461633f4124db66f
Lukasz Siwinski <lsiwinski@gmail.com>
2013-11-28 19:57:25 +0100

    links to social networks

---
+++ javascripts/SocNets.js
@@ -0,0 +1,20 @@
+function SocialNetworks($scope) {
+// ą ć ę ł ń ó ś ź ż
+	$scope.socnets = [ 
+	{
+		id : "twitter", 
+		link : "https://twitter.com/gitkata", 
+		title : "Git Kata on Twitter"
+	},
+	{
+		id : "google-plus", 
+		link : "https://plus.google.com/105116905624196940229/posts", 
+		title : "Git Kata on Google Plus"
+	},
+	{
+		id : "facebook", 
+		link : "https://www.facebook.com/gitkatawarsaw", 
+		title : "Git Kata on Facebook"
+	}
+	];
+}
